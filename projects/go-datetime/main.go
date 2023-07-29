83a9b19589e1347d816875334eb1599042c56ce5
venzel <eneas.eng@yahoo.com>
2023-07-28 21:16:17 -0300

    feat: continua evoluindo o repositório

---
+++ projects/go-datetime/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"datetime/order"
+	"fmt"
+	"os"
+	"time"
+)
+
+func init() {
+	os.Setenv("TZ", "UTC")
+}
+
+func main() {
+	product, _ := order.NewProduct("Leite", "tiago@gmail.com.br", 190.45)
+
+	fmt.Println(product)
+
+	now := time.Now()
+
+	day, month, year := now.Day(), int(now.Month()), now.Year()
+
+	fmt.Println(day, month, year)
+
+	fmt.Println(now.Unix()) // Timestamps in seconds
+	fmt.Println(now.Format("02-01-2006 15:04:05"))
+
+	p1, _ := order.NewProduct("Leite", "tiago@gmail.com.br", 190.45)
+	p2, _ := order.NewProduct("Leite", "tiago@gmail.com.br", 190.45)
+	p3, _ := order.NewProduct("Leite", "tiago@gmail.com.br", 190.45)
+
+	products := order.Products{p1, p2, p3}
+
+	dateCreated := time.Date(2023, 8, 30, 0, 0, 0, 0, time.UTC)
+
+	for _, product := range products {
+		if product.CreatedAt.After(dateCreated) {
+			fmt.Println(product)
+		}
+	}
+}
---
+++ projects/go-datetime/order/product.go
@@ -0,0 +1,51 @@
+package order
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID        string    `json:"id" valid:"uuid"`
+	Name      string    `json:"name" valid:"required,stringlength(5|255)"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+}
+
+type Products []*Product
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewProduct(name, email string, price float64) (*Product, error) {
+	product := &Product{
+		Name: name,
+	}
+
+	product.prepare()
+
+	err := product.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *Product) prepare() {
+	p.ID = uuid.New().String()
+	p.CreatedAt = time.Now()
+}
+
+func (p *Product) isValid() error {
+	_, err := govalidator.ValidateStruct(p)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
